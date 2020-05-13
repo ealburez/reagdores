dfdabcad1319829d938c43eb0b745fa71207c96c
ealburez <61049785+ealburez@users.noreply.github.com>
2020-05-13 11:23:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,45 @@ function changeColor(newColor) {
   var elem = document.getElementById('text1');
   elem.style.color = newColor;
 }
+
 function updateDate() {
   changeColor('black');
-  var dt = new Date();
-  var arreglo = document.getElementById("hora1").value.split(":")
-  var delta = parseInt(document.getElementById("tiempo1").value);
-  var stopTime
-  var output 
+  //----Variables de html
+  var numRega =  parseInt(document.getElementById("numRega").textContent)
+  var delta;		//dieferencia de tiempo que debe ser
+  var hora; 		//hora de inicio del riego
   
-  dt.setHours(parseInt(arreglo[0]));
-  dt.setMinutes(parseInt(arreglo[1]));
-  dt.setTime (dt.getTime() + delta*60000);
-  stopTime = [dt.getHours(),dt.getMinutes()]
+  // Otras variables
+  var dt = new Date();
+  var currentDate = new Date();
+  var i = 1;
+  var stopTime;
+  var output;  
+  var lista =["hora"]
+  
+  //poner el for aquí
+  hora = document.getElementById("hora"+i).value.split(":");
+	delta = parseInt(document.getElementById("tiempo"+i).value);
 
-  if (stopTime[0]<10){
-  	output = "0" + stopTime[0] + ":"
+  dt.setHours(parseInt(hora[0]));
+  dt.setMinutes(parseInt(hora[1]));
+  dt.setTime(dt.getTime() + delta * 60000);
+  stopTime = [dt.getHours(), dt.getMinutes()]
+
+  if (stopTime[0] < 10) {
+    output = "0" + stopTime[0] + ":"
   } else {
-  	output = stopTime[0] + ":"
+    output = stopTime[0] + ":"
   }
-  if (stopTime [1]<10){
-  	output += "0" + stopTime[1]
+  if (stopTime[1] < 10) {
+    output += "0" + stopTime[1]
   } else {
-  	output += stopTime[1]
+    output += stopTime[1]
   }
-  document.getElementById("fecha0").innerHTML = dt.toLocaleString();
+  
+  document.getElementById("confirm1").innerHTML = "Guardado:" + currentDate.toLocaleString();
   document.getElementById("tiempo2").innerHTML = output;
   //document.getElementById("tiempo1").value = 10;
   //document.getElementById("hora1").value = "02:08"
   //document.getElementById("hora1").value = output
-  
-}
+
