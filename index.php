6d8698d27e20fac022d23f4bf2d9e56cf6dd2828
ealburez <61049785+ealburez@users.noreply.github.com>
2020-05-14 14:32:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 		
   		/*Creación de los controles de los regadores*/
 		
-			echo "<label for='button_$i'>Regador $i </label><input type='checkbox' id='estado_$i' name='estado_$i' $checa><br>";
-			echo "<button onclick='changeColor('yellow');' id='button_$i'><br><br>'Encender 5 minutos'</button>";
+			echo "<label for='estado_$i'>Regador $i </label><input type='checkbox' id='estado_$i' name='estado_$i' $checa><br>";
+			echo "<button onclick='changeColor('yellow');' id='button_$i'>'Encender 5 minutos'</button><br><br>";
 			echo "<label for='hora_$i'>Hora de inicio</label><input type='time' id='hora_$i' value='12:00' name='hora_$i'> <br>";
 			echo "<label for='tiempo_$i'>Tiempo (en minutos):</label><input type='number' id='tiempo_$i' name='tiempo_$i' value='10' min='1' max='100'><br>";
 	}
