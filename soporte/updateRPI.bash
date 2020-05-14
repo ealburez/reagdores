08256ded9e1ed44885bd8b74543cd019b9e91ff2
ealburez <61049785+ealburez@users.noreply.github.com>
2020-05-14 11:31:00 +0200

    Update updateRPI.bash

--- soporte/updateRPI.bash
+++ soporte/updateRPI.bash
@@ -5,6 +5,7 @@
 
 #variables
 repositoryDir="/home/repo/regadores"
+wwwDir="/var/www/html"
 
 #Update the iota repository
 git -C $repositoryDir reset --hard
@@ -12,6 +13,10 @@ git -C $repositoryDir pull
 
 #make all .bash files excecutable
 sudo chmod ugo+x $repositoryDir/*.bash
+
+#move all pages to it position
+find $repositoryDir -maxdepth 1 -type f -exec cp {} $wwwDir \;
+
 
 #Update Crontab
 #crontab buCrontab
