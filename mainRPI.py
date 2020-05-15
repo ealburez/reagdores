d8bb84f6bbfa3c97723888e0f470bcf0b7ea8673
enio <ealburez@blabla.com>
2020-05-15 21:35:06 +0200

    test

--- mainRPI.py
+++ mainRPI.py
@@ -8,7 +8,7 @@ with open('regadores.conf') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         for x in range (2,3):
-        if !row[x].replace(":","").isdigit():
-            row[x]="00:00"
+            if !row[x].replace(":","").isdigit():
+                row[x]="00:00"
             
         print(row)
