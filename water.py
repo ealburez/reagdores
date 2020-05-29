8808cdeb512e9d7b7bdd409b675fcff3d601dc86
ealburez <ealburez@gmail.com>
2020-05-29 09:23:35 +0200

    added water.py

---
+++ water.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import sys, time
+
+#------------------------------
+#	varaibles
+#------------------------------
+
+outPin = [2,3,4]
+try:
+	sleepTime = argv[1]*60
+except:
+	pass
+
+#------------------------------
+#	Funciones
+#------------------------------
+
+def setRegador (idTag,waitTime)
+	print "encendiendo",idTag, waitTime
+	GPIO.output(idTag, GPIO.HIGH) #Turn on
+	time.sleep(waitTime)	#wait until shutting sprinkler off
+	GPIO.output(led, GPIO.LOW) #Turn off
+
+
+
+#------------------------------
+#	Program
+#------------------------------
+GPIO.setmode(GPIO.BOARD)
+
+#---Apagar todos los regadores---
+for i in range(len(outPin)):
+	GPIO.setup(i,GPIO.OUT)	#Define as output
+	GPIO.output(i, GPIO.LOW) #Turn off
+	print "apaga" , i
+
+#---Prender los regadores en secuencia
+
+if len(sys.argv)==2:
+	for i in range(len(outPin)):
+		setRegador(outPin[i],sleepTime)
+
+#---Prender un regador
+elif len(sys.argv)==4:
+	setRegador(argv[3],sleepTime)
+	#logica para programar un regador
